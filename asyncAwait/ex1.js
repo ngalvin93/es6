f1c65b9c5bde08da3ab0772a144f6119f5f433c2
Alvin Ng <ngalvin93@gmail.com>
2019-10-29 23:50:25 -0500

    async complete

--- asyncAwait/ex1.js
+++ asyncAwait/ex1.js
@@ -9,3 +9,15 @@ let getDogsOwnersBestFriend = () => {
         return friend.name;
     })
 }
+
+// es6
+let getDogsOwnersBestFriend = async () => {
+    try {
+        const response1 = await get('dog/1');
+        const response2 = await get(`/owner/${response1.owner}`);
+        const response3 = await get(`/friend/${response2.bestFriend}`);
+        return response3.name;
+    } catch (error) {
+        console.log(error);
+    }
+}
