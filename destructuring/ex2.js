0ac92f1241650f2d9f9f08bb7acd51ebefa2b1b0
Alvin Ng <ngalvin93@gmail.com>
2019-10-30 22:25:32 -0500

    ok

--- destructuring/ex2.js
+++ destructuring/ex2.js
@@ -8,4 +8,5 @@ let array = [
 ] 
 // Using destructuring, and in one line of code, 
 //  create 2 variables that extract blue, green, and pink
-
+let [one, two, , three] = array
+console.log(one, two, three)
