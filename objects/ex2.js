604324dd68f3d57f5190e3ebf69b4655ff7f9663
Alvin Ng <ngalvin93@gmail.com>
2019-10-31 21:34:53 -0500

    question

--- objects/ex2.js
+++ objects/ex2.js
@@ -3,3 +3,7 @@ let values = ['val1', 'val2', 'val3'];
 
 
 // Using a single object literal, create an object that maps key1, key2, and key3 to val1, val2, and val3, respectively
+
+let object = {
+    
+}
--- variables/ex1.js
+++ variables/ex1.js
@@ -1,6 +1,16 @@
 // Declare and instantiate a block scoped variable
 // YOUR CODE GOES HERE
-
+const func = number => {
+    let total = []
+    total.push(number + 1)
+    console.log(total)
+}
+func(5)
 
 // Declare and instantiate a unchangeable block scoped variable
 // YOUR CODE GOES HERE
+const func2 = (num1, num2) => {
+    const num3 = 10
+    console.log(num1+num2+num3)
+}
+func2(5, 2)
