9ef340c1aebd82f5d69458896ea6bd30d6550e1b
Alvin Ng <ngalvin93@gmail.com>
2019-10-31 20:58:52 -0500

    module

--- modules/ex1.js
+++ modules/ex1.js
@@ -1,2 +1,4 @@
 // Write a statement that imports every value from "export.js"
 
+import { PI as pie, add, bool } from './export';
+console.log(pie, add, bool)
--- modules/ex2.js
+++ modules/ex2.js
@@ -1,1 +1,5 @@
 // export two numbers and a function from this file. Check "import.js" to see how you should name these variables
+
+export const func = (arg1,arg2) => arg1 +arg2;
+export const arg1 = 5;
+export const arg1 = 4;
